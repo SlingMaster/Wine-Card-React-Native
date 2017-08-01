9980f388ab603caa0e239346e59aa722ff611b7a
SlingMaster <alexdovby@gmail.com>
2017-08-01 09:09:57 +0300

    clear code

--- App.js
+++ App.js
@@ -22,35 +22,11 @@ var WineList = require('./components/wine_list/WineList');
 // this.props.navigation.dispatch(WineCard)
 
 
-// class RecentChatsScreen extends React.Component {
-//   render() {
-//     return <Text>List of recent chats</Text>
-//   }
-// }
-
-// class AllContactsScreen extends React.Component {
-//   render() {
-//     return <Text>List of all contacts</Text>
-//   }
-// }
-
-// class redWineList extends React.Component {
-//   render() {
-//     return <Text>List of all contacts</Text>
-//   }
-// }
 
 // const WineCard = TabNavigator({
 //   Splash: { screen: Splash },
 //   HomeScreen: { screen: HomeScreen },
 //   WineList: { screen: WineList },
-// });
-
-// const WineCard = StackNavigator({
-//   Splash: { screen: Splash },
-//   WineList: { screen: WineList },  
-//   HomeScreen: { screen: HomeScreen },
-
 // });
 
 
